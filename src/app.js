68b871cfe9535b45e39570d9ce676e362bc14281
Rodrigo Borges <digoborges999@gmail.com>
2026-03-12 22:10:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,14 @@ app.get("/", (request, response) => {
     response.send("Olá mundo.")
 })
 
+app.use(express.json())
+
+app.post("/users", (req,res) =>{
+    console.log(req.body)
+
+    res.send("Estamos testando")
+})
+
 app.get("/users",userController.getAllUsers )
 
 module.exports = app
